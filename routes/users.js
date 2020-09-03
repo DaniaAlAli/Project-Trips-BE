23496c919798c4bc8aa3b057cbbfdcb4629298c4
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2020-09-02 17:58:43 -0700

    fetch Profile

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin } = require("../controllers/userController");
+const { signup, signin, userList } = require("../controllers/userController");
 const passport = require("passport");
+
+router.param("userId", async (req, res, next, userId) => {
+  const user = await fetchUser(userId, next);
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    const err = new Error("User is not found");
+    err.status = 404;
+    next(err);
+  }
+});
+
+router.get("/profiles", userList);
 
 // Sign up
 router.post("/signup", signup);
